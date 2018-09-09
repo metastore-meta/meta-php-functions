5930ebf17c7e6040950f033c2d8be2eae748b4aa
Kitsune Solar <kitsune.solar@gmail.com>
2018-09-09 16:45:55 +0300

    2018-09-09 13:45:55

--- common/common.out.cr.php
+++ common/common.out.cr.php
@@ -8,7 +8,7 @@ class Class_CR {
 	 * @return mixed
 	 */
 	public static function outCR() {
-		$cr  = 'PHNwYW4gY2xhc3M9Im1ldGFkYXRhLWNyIj5NYWRlIHdpdGggPHNwYW4gY2xhc3M9ImZhcyBmYS1oZWFydCI+PC9zcGFuPiBieSA8YSB0aXRsZT0iV2ViIEFwcGxpY2F0aW9uIERldmVsb3BtZW50IENvbXBhbnkiIGhyZWY9Imh0dHBzOi8vbWV0YWRhdGEuZm91bmRhdGlvbi8iPjxzdHJvbmc+TUVUQURBVEE8L3N0cm9uZz48L2E+PC9zcGFuPg==';
+		$cr  = 'PHNwYW4gY2xhc3M9Im1ldGEtY3IiPjxpIGNsYXNzPSJmYXMgZmEtY29kZSI+PC9pPiB3aXRoIDxpIGNsYXNzPSJmYXMgZmEtaGVhcnQiIHN0eWxlPSJjb2xvcjogI2ZlNmUzYTsiPjwvaT4gYnkgPGEgdGl0bGU9IldlYiBBcHBsaWNhdGlvbiBEZXZlbG9wbWVudCBDb21wYW55IiBocmVmPSJodHRwczovL21ldGFkYXRhLmZvdW5kYXRpb24vIj48c3Ryb25nPk1FVEFEQVRBPC9zdHJvbmc+PC9hPjwvc3Bhbj4=';
 		$out = base64_decode( $cr );
 
 		return $out;
